4d44f5b9431222ddbdc57576c726d3bd4fcf3537
thecurler <gunnar.s.gs@gmail.com>
2016-05-06 11:39:38 +0200

    Update script-1.js
    15 Serien aufeinmal speichern

--- script-1.js
+++ script-1.js
@@ -19,7 +19,11 @@ const gInfo = "***Info***\n - Dies ist ein Script um Serien zu speichern und wei
                    + "*Löschen*\n - Löscht den Cookie.\n\tGeht nur, wenn ein Cookie gespeichert ist.\n\n"
                    + "*Stop*\n - Stoppt den Autoplay.\n\tZwischen den Videos hat man 6-Sekunden, um auf den Button zu klicken.\n\tDanach kann man, mit dem \"weiter gucken\" - Button, die nächste Folge wie gewohnt abspielen.\n\n"
                    + "Viel Spaß und immer harte Curls, Gunnar\n\n"
-                   + "Deine Serie ist gerade: " + get_cookie ("continue");
+                   + "Deine Serie ist gerade: " + get_cookie ("continue") + "\n\n"
+                   + "NEU!!!! Jetzt kann man auch mehere Serien speichern. Bis zu 15 Stück!\n\n"
+                   + "Bsp.: Wähle eine Serie deiner wähl(South-Park). Wähle eine  Folge aus(Staffel 1, Folge 1).Drücke den Speichern Button. Jetzt haben wir die Folge in einem Cookie gespeichert.Es können bis zu 15 Serien gleichzeitig gespeichert werden\n\n"
+                   + "Als nächestes klicken wir auf den weiter gucken Button. Das war es schon!!!!!!\n\n"
+                   + "Jetzt läuft die Serien durch xD. Folge für Folge.\n\n";
 //
 //class button
 function button (value, x, y, click, can)
@@ -53,11 +57,50 @@ function button (value, x, y, click, can)
     document.body.appendChild (input);
 }
 //
+//clas select
+function cookie_switch (str)
+{
+    var list = cookie_list ();
+    var x = document.createElement ("SELECT");
+    for (var i = 0; i < list.length; i++)
+    {
+        var opt = list[i].split ("=", 30)
+        var z = document.createElement ("option");
+        z.setAttribute ("value", opt[1]);
+        z.appendChild (document.createTextNode (opt[0]));
+        x.appendChild (z);
+    }
+    if (str == ": Cookie löschen?")
+        x.onclick = function (){ if (confirm (x.children[x.selectedIndex].text + str)) del_cookie (x.children[x.selectedIndex].text); location.reload ();};
+    else
+       x.onclick = function (){ if (confirm (x.children[x.selectedIndex].text + str)) next (x.value)};
+    x.style.padding = "10px 22px";
+    x.style.fontSize = "22px";
+    x.setAttribute ("size", list.length);
+    x.style.top = "5px";
+    x.style.left = "300px";
+    x.style.zIndex = "9999999";
+    x.style.position = "fixed";
+    document.body.appendChild(x);
+}
+//
+//cookie list
+function cookie_list ()
+{
+    var list = document.cookie.split (";", 15);
+    var new_list = [];
+    for (var i = 0; i < list.length; i++)
+        if (list[i].match ("serie") && !list[i].match ("continue"))
+            new_list[new_list.length] = list[i];
+    return new_list;
+
+}
+//
 //buttons
 var info_button = new button ("info", "5px", "5px", function (){ alert (gInfo);}, function (){ return 1;});
 var safe_button = new button ("speichern", "60px", "5px", function (){ if (confirm ("Speichern?")){ save (); location.reload ();}}, function (){ if (is_valid (location.href)) return 1;});
-var next_button = new button ("weiter gucken", "115px", "5px", function (){ if (confirm ("Weiter gucken?")) next ();}, function (){ if (get_cookie ("continue")) return 1;});
-var delete_button = new button ("löschen", "170px", "5px", function (){ if (confirm ("Löschen?")){ del_cookie ("continue"); location.assign (location.origin);}}, function (){ if (get_cookie ("continue")) return 1;});
+var next_button = new button ("weiter gucken", "115px", "5px", function (){ cookie_switch (": Weiter gucken?");}, function (){ if (cookie_list () != "") return 1;});
+var delete_button = new button ("löschen", "170px", "5px", function (){ cookie_switch (": Cookie löschen?");}, function (){ if (cookie_list () != "") return 1;});
 var stop_button =  new button ("stop", "225px", "5px", function (){ if (confirm ("Stop")){ clearTimeout (gTimeout); location.assign (location.origin);}}, function (){ if (location.search == "?next") return 1;});
 //
 //forever
@@ -66,16 +109,19 @@ window.onload = forever ();
 //forever
 function forever ()
 {
+    //serie ends
     if (get_cookie ("continue") == "end" && ! (location.pathname.match ("/Vivo-1")))
     {
         del_cookie ("continue");
         alert( "Serie zu Ende" );
         location.assign (location.origin);
     }
+    //start video
     else if (location.pathname.match ("/Vivo-1") && location.search == "?next")
         location.assign (document.getElementById ("video_actions").firstElementChild.firstElementChild);
+    //after video
     else if (location.search == "?next")
-        gTimeout = window.setTimeout (function (){ next ();}, 3000);
+        gTimeout = window.setTimeout (function (){ next ("continue");}, 3000);
 }
 //
 //delete cookie
@@ -113,13 +159,21 @@ function get_cookie (cname)
 }
 //
 //play next episode
-function next ()
+function next (url)
 {
-    var url = get_cookie ("continue");
+    //after vid
+    if(url == "continue")
+       url = get_cookie ("continue");
+    //if button clicked
     if (! (location.pathname.match ("/serie/")))
+    {
+        set_cookie ("continue", url, 50);
         location.assign (url.slice (0, url.indexOf ("/Vivo-1")) + "?next");
+    }
+    //save next episode
     var next_ep = get_next_ep ();
     set_cookie ("continue", next_ep, 50);
+    set_cookie (url.split ("/", 5)[4], next_ep, 50);
     location.assign (url);
 }
 //
@@ -180,7 +234,7 @@ function save ()
     var url = location.href;
     if (! (url = is_valid (url)))
         return;
-    set_cookie ("continue", url , 50);
+    set_cookie (url.split ("/", 5)[4], url , 50);
 }
 //
 //is given url valid to save
